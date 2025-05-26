b50e28064942a52dd3faa3260ad284dc0a651187
Huy-DaraPichchan <darapichchan.huy@gmail.com>
2025-05-26 21:45:09 +0900

    add meta tags

--- root/settings.py
+++ root/settings.py
@@ -115,10 +115,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+print("Base dir ", BASE_DIR)
+print("static root", STATIC_ROOT)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
